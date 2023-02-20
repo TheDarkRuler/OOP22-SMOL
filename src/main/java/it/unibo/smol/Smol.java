de8e44f71be8956562e7388ba1631e1b4ca5f4fc
Marco Galeri <marco.galeri@studio.unibo.it>
2023-02-20 18:14:01 +0100

    Clean and organize the workspace

---
+++ src/main/java/it/unibo/smol/Smol.java
@@ -0,0 +1,10 @@
+package it.unibo.smol;
+
+import javafx.application.Application;
+
+public final class Smol {
+    
+    public static void main(String[] args) {
+        Application.launch(SmolApplication.class, "");
+    }
+}
---
+++ src/main/java/it/unibo/smol/SmolApplication.java
@@ -0,0 +1,13 @@
+package it.unibo.smol;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class SmolApplication extends Application{
+
+    @Override
+    public void start(Stage primaryStage) {
+        //new Window().launch();
+    }
+    
+}
