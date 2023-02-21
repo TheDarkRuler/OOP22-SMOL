209271cee8a2ecf1c4d55f089a893bf7759b6c06
Marco Galeri <marco.galeri@studio.unibo.it>
2023-02-21 17:16:16 +0100

    Add the GameEngine

---
+++ src/main/java/it/unibo/smol/core/GameEngine.java
@@ -0,0 +1,57 @@
+package it.unibo.smol.core;
+
+/**
+ * This class is the engine of the game.
+ * The purpose of the engine is to control the {@link GameLoop} thread
+ */
+public class GameEngine {
+
+    private GameLoop gameLoop;
+
+    /**
+     * Rappresent the state of the {@link GameLoop}.
+     * {@code True} if is running; {@code False} otherwise
+     */
+    private boolean state;
+
+    /**
+     * Awake the {@link GameLoop} thread with a {@link #notify()}.
+     * @throws IllegalStateException if the thread was already running when the method was called
+     */
+    public void run() {
+        if (this.isRunning()) {
+            throw new IllegalStateException("GameLoop is already running");
+        }
+        gameLoop.notify();
+    }
+
+    /**
+     * Stop the {@link GameLoop} thread with a {@link #wait()}.
+     * @throws IllegalStateException if the thread was already stopped when the method was called
+     * @throws InterruptedException if any thread interrupted the current thread before or
+     *         while the current thread was waiting.
+     */
+    public void stop() throws InterruptedException {
+        if (!this.isRunning()) {
+            throw new IllegalStateException("GameLoop is alredy stopped");
+        }
+        gameLoop.wait();
+    }
+
+    /**
+     * Test if the {@link GameLoop} Thread is running.
+     * @return {@code True} if is running; {@code False} otherwise
+     */
+    public boolean isRunning() {
+        return state;
+    }
+
+    /**
+     * This method create a and start a new {@link GameLoop}.
+     */
+    public void init() {
+        state = true;
+        gameLoop = new GameLoop();
+        gameLoop.start();
+    }
+}
