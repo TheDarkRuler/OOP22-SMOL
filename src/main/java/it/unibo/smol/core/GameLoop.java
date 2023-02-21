5b5c0bf48f920ebcc9cfc833341ab8340f7d4860
Marco Galeri <marco.galeri@studio.unibo.it>
2023-02-21 20:13:08 +0100

    Fix almost all checkStyle issues

--- src/main/java/it/unibo/smol/core/GameLoop.java
+++ src/main/java/it/unibo/smol/core/GameLoop.java
@@ -1,13 +1,16 @@
 package it.unibo.smol.core;
 
-public class GameLoop extends Thread{
+/**
+ * This class is the responsible of the overall flow control of the game.
+ */
+public class GameLoop extends Thread {
 
     private static final int FPS = 100;
-    private static final double drawInterval = 1000000000/FPS;
+    private static final double DRAW_INTERVAL = 1000000000 / FPS;
 
-    private long lastTime=0;
-    private long currentTime=0;
-    private double delta=0;
+    private long lastTime = 0;
+    private long currentTime = 0;
+    private double delta = 0;
 
     /*long timer =0;
     int drawCount=0;*/
@@ -15,14 +18,12 @@ public class GameLoop extends Thread{
     @Override
     public void run() {
          lastTime = System.nanoTime();
-        while(this != null) {
-            
+        while (this != null) {
             if (syncFrame()) {
                 //drawCount++;
                 processInput();
                 update();
                 repaint();
-                
             }
             /*if (timer >= 1000000000) {
                 System.out.println("FPS: "+ drawCount);
@@ -32,28 +33,41 @@ public class GameLoop extends Thread{
         }
     }
 
+    /**
+     * update.
+     */
     public void update() {
 
     }
 
+    /**
+     * process.
+     */
     private void processInput() {
 
     }
 
+    /**
+     * repaint.
+     */
     private void repaint() {
 
     }
 
+    /**
+     *  Syncronize the {@link GameLoop} with the desired FPS rating ({@value #FPS}).
+     * @return {@code True} if the time passed is equal or more of the desired {@link #DRAW_INTERVAL};
+     * Otherwise {@code False}.
+     */
     private boolean syncFrame() {
         currentTime = System.nanoTime();
-        
-        delta += (currentTime - lastTime) / drawInterval;
+        delta += (currentTime - lastTime) / DRAW_INTERVAL;
         //timer += (currentTime - lastTime);
         lastTime = currentTime;
-        if(delta >= 1) {
+        if (delta >= 1) {
             delta--;
             return true;
         } 
         return false;
-    }  
+    }
 }
