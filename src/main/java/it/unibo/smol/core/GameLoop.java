371e8c263e96d75bf19bbffd4fe08301eb6aa940
Marco Galeri <marco.galeri@studio.unibo.it>
2023-03-03 18:50:54 +0100

    gameloop non funzionante da fixare :c

--- src/main/java/it/unibo/smol/core/GameLoop.java
+++ src/main/java/it/unibo/smol/core/GameLoop.java
@@ -6,15 +6,18 @@ package it.unibo.smol.core;
 public class GameLoop extends Thread {
 
     private static final int FPS = 100;
-    private static final double DRAW_INTERVAL = 1_000_000_000 / FPS;
+    private static final double FPS_INTERVAL = 1_000_000_000 / FPS;
 
-    private long lastTime;
-    private double delta;
+    private static final int UPS = 100;
+    private static final double UPS_INTERVAL = 1_000_000_000 / UPS;
+
+    private long lastTimeFPS, lastTime;
+    private double deltaFPS=0, deltaUPS=0;
     private boolean end;
 
     // ALL commented code is for FPS checking
-    /*long timer =0;
-    int drawCount=0;*/
+    long timer =0;
+    int drawCount=0;
 
     /**
      * Override of the {@code run()} method in the {@link Thread} class.
@@ -22,19 +25,25 @@ public class GameLoop extends Thread {
      */
     @Override
     public void run() {
+         lastTimeFPS = System.nanoTime();
          lastTime = System.nanoTime();
         while (!end) {
-            if (syncFrame()) {
-                //drawCount++;
+
+            if (syncTime(UPS_INTERVAL, deltaUPS)) {
+                drawCount++;
                 processInput();
                 update();
-                repaint();
             }
-            /*if (timer >= 1000000000) {
-                System.out.println("FPS: "+ drawCount);
+
+            /*if (syncTime(FPS_INTERVAL, deltaFPS, lastTimeFPS)) {
+                repaint();
+            }*/
+                
+            if (timer >= 1000000000) {
+                System.out.println("UPS: "+ drawCount);
                 drawCount=0;
                 timer=0;
-            }*/
+            }
         }
     }
 
@@ -64,15 +73,17 @@ public class GameLoop extends Thread {
      * @return {@code True} if the time passed is equal or more of the desired {@link #DRAW_INTERVAL};
      * Otherwise {@code False}.
      */
-    private boolean syncFrame() {
+    private boolean syncTime(final double interval, double delta) {
         final long currentTime = System.nanoTime();
-        delta += (currentTime - lastTime) / DRAW_INTERVAL;
-        //timer += (currentTime - lastTime);
+        delta += (currentTime - lastTime) / interval;
+        timer += (currentTime - lastTime);
         lastTime = currentTime;
+        System.out.println(delta);
+        System.out.println(deltaUPS);
         if (delta >= 1) {
             delta--;
             return true;
-        } 
+        }
         return false;
     }
 }
