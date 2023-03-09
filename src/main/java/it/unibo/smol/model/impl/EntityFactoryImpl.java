0142ea79aab34df4d3a356369e775e9884226ae8
Marco Galeri <marco.galeri@studio.unibo.it>
2023-03-09 16:21:48 +0100

    Partially create Entity Factory

--- src/main/java/it/unibo/smol/model/impl/EntityFactoryImpl.java
+++ src/main/java/it/unibo/smol/model/impl/EntityFactoryImpl.java
@@ -1,44 +1,60 @@
 package it.unibo.smol.model.impl;
 
-import java.util.List;
+import java.util.Optional;
 
+import it.unibo.smol.model.Type;
 import it.unibo.smol.model.api.*;
-
+/**
+ * Implemention of the {@link EntityFactory} interface.
+ */
 public class EntityFactoryImpl implements EntityFactory {
+
     /**
      * {@inheritDoc}
      */
     @Override
-    public List<Entity> Moles() {
-        // TODO Moles
-        throw new UnsupportedOperationException("Unimplemented method 'Moles'");
+    public Entity createBasicMole(double x, double y) {
+        return new EntityImpl(Type.ENEMY, null, Optional.of(new HealthComponent(1)), null, null, x, y);
     }
 
     /**
      * {@inheritDoc}
      */
     @Override
-    public Entity Player() {
-        // TODO Player
-        throw new UnsupportedOperationException("Unimplemented method 'Player'");
+    public Entity createPlayer(double x, double y) {
+        return new EntityImpl(Type.PLAYER, null, Optional.empty(), null, null, x, y);
     }
 
     /**
      * {@inheritDoc}
      */
     @Override
-    public List<Entity> LifePlants() {
-        // TODO LifePlants
-        throw new UnsupportedOperationException("Unimplemented method 'LifePlants'");
+    public Entity createLifePlants(double x, double y) {
+        return new EntityImpl(Type.HEALTH, null, Optional.of(new HealthComponent(1000)), null, null, x, y);
     }
 
     /**
      * {@inheritDoc}
      */
     @Override
-    public Entity Weapon() {
-        // TODO Weapon
-        throw new UnsupportedOperationException("Unimplemented method 'Weapon'");
+    public Entity createWeapon(double x, double y) {
+        return new EntityImpl(Type.WEAPON, null, Optional.empty(), null, null, x, y);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Entity createElmetMole(double x, double y) {
+        return new EntityImpl(Type.ENEMY, null, Optional.of(new HealthComponent(2)), null, null, x, y);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Entity createAngryMole(double x, double y) {
+        return new EntityImpl(Type.ENEMY, null, Optional.of(new HealthComponent(1)), null, null, x, y);
     }
     
 }
