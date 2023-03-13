0057d91f9f9ed30da1f3b541e8312aa032322350
Giovanni Paradisi <giovanni.paradisi2@studio.unibo.it>
2023-03-13 19:58:06 +0100

    create GameState interface  and impl

--- src/main/java/it/unibo/smol/model/api/GameState.java
+++ src/main/java/it/unibo/smol/model/api/GameState.java
@@ -1,14 +1,37 @@
 package it.unibo.smol.model.api;
-
+/**
+ *  Interface for control the state of the game.
+ */
 public interface GameState {
     /**
+     * Get the current world.
      * @return game world
      */
     World getWorld();
 
     /**
-     * @return if game is Over or not
+     * @return if game is Over or not.
      */
     boolean isGameOver();
 
+    /**
+     *  Notify the world to increase the score.
+     */
+    void incScore();
+
+    /**
+     * Notify the world to decrease the score.
+     */
+    void decScore();
+
+    /**
+     * Getter for the score.
+     * @return the current score
+     */
+    int getScore();
+
+    /**
+     *  Notify the world to kill entities.
+     */
+    void notifyDeath();
 }
