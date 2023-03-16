c622a36153baf802b6fdb9114b79449703af38d9
Marco Galeri <marco.galeri@studio.unibo.it>
2023-03-16 16:47:53 +0100

    Fix JavaDoc

--- src/main/java/it/unibo/smol/common/Constant.java
+++ src/main/java/it/unibo/smol/common/Constant.java
@@ -8,40 +8,40 @@ import java.io.UnsupportedEncodingException;
 public final class Constant {
     /**player width. */
     public static final int PLAYER_WIDTH = 90;
-    /**player width. */
+    /**player height. */
     public static final int PLAYER_HEIGHT = 160;
-    /**player width. */
+    /**player movement speed. */
     public static final double PLAYER_MOVSPD = 5.0;
-    /**player width. */
+    /**player damage. */
     public static final int PLAYER_DMG = -1;
 
-    /**player width. */
+    /**enemy width. */
     public static final int ENEMY_WIDTH = 90;
-    /**player width. */
+    /**enemy height. */
     public static final int ENEMY_HEIGHT = 160;
-    /**player width. */
+    /**enemy movement speed. */
     public static final double ENEMY_MOVSPD = 5.0;
-    /**player width. */
+    /**enemy health point. */
     public static final int ENEMY_HP = 100;
-    /**player width. */
+    /**enemy with helmet health point. */
     public static final int ENEMY_HELMET_HP = 200;
-    /**player width. */
+    /**enemy damage. */
     public static final int ENEMY_DMG = -1;
 
-    /**player width. */
+    /**lifeplant width. */
     public static final int HEALTH_WIDTH = 180;
-    /**player width. */
+    /**lifeplant height. */
     public static final int HEALTH_HEIGHT = 360;
-    /**player width. */
+    /**lifeplant movement speed. */
     public static final double HEALTH_MOVSPD = 0.0;
-    /**player width. */
+    /**lifeplant health point. */
     public static final int HEALTH_HP = 1000;
 
-    /**player width. */
+    /**weapon radius. */
     public static final int WEAPON_RADIUS = 20;
-    /**player width. */
+    /**weapon movement speed. */
     public static final double WEAPON_MOVSPD = 0.0;
-    /**player width. */
+    /**weapon damage. */
     public static final int WEAPON_DMG = -100;
 
     private Constant() throws UnsupportedEncodingException {
