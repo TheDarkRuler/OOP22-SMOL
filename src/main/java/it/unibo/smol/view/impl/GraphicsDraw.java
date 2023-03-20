e7c12ebe79c2f53d6537d173bcbf067fc5a467b7
Ettore Farinelli <ettore.farinelli@studio.unibo.it>
2023-03-20 18:57:13 +0100

    started to implement the draw og the objects

---
+++ src/main/java/it/unibo/smol/view/impl/GraphicsDraw.java
@@ -0,0 +1,17 @@
+package it.unibo.smol.view.impl;
+
+import javafx.scene.canvas.GraphicsContext;
+
+public class GraphicsDraw {
+
+    private final GraphicsContext g;
+
+    public GraphicsDraw(final GraphicsContext g) {
+        this.g = g;
+    }
+
+    public void drawSprite() {
+        
+    }
+    
+}
