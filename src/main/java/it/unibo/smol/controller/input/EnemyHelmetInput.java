44b19c7880209373c19f1daa9876a8176f7d30cc
Ettore Farinelli <ettore.farinelli@studio.unibo.it>
2023-03-21 16:02:53 +0100

    changed name error

--- src/main/java/it/unibo/smol/controller/input/EnemyHelmetInput.java
+++ src/main/java/it/unibo/smol/controller/input/EnemyHelmetInput.java
@@ -6,15 +6,15 @@ import it.unibo.smol.model.api.GameState;
 /**
  * creates a enemy with elmet.
  */
-public class EnemyElmetInput extends EnemyInput {
+public class EnemyHelmetInput extends EnemyInput {
 
-    private static final int ELMET_ENEMY_MAX_SPAWNS = 3;
+    private static final int HELMET_ENEMY_MAX_SPAWNS = 3;
 
     /**
      * gives the enemy Max times spawn and the given game state.
      * @param gs
      */
-    public EnemyElmetInput(final GameState gs) {
-        super(ELMET_ENEMY_MAX_SPAWNS, gs);
+    public EnemyHelmetInput(final GameState gs) {
+        super(HELMET_ENEMY_MAX_SPAWNS, gs);
     }
 }
