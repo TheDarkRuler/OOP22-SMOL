2a81a6ceac31f09a315d8824bd63a755bc7f2acc
Ettore Farinelli <ettore.farinelli@studio.unibo.it>
2023-03-27 18:40:03 +0200

    now the hammer hits just one time

--- src/main/java/it/unibo/smol/controller/input/MouseInputs.java
+++ src/main/java/it/unibo/smol/controller/input/MouseInputs.java
@@ -22,6 +22,7 @@ public class MouseInputs implements EventHandler<MouseEvent> {
     private boolean weaponSmashed;
     private boolean weaponIsSmashing;
     private boolean cursorOnScreen;
+    private boolean weaponHits;
     private int weaponIncrease;
     private int weaponRange;
     private final ScheduledExecutorService animationTime;
@@ -32,6 +33,7 @@ public class MouseInputs implements EventHandler<MouseEvent> {
      */
     public MouseInputs() {
         this.weaponSmashed = false;
+        this.weaponHits = false;
         this.weaponIsSmashing = false;
         this.weaponRange = 0;
         this.weaponIncrease = 0;
@@ -52,6 +54,7 @@ public class MouseInputs implements EventHandler<MouseEvent> {
         } else if (event.getEventType().equals(MouseEvent.MOUSE_RELEASED) && !this.weaponSmashed) {
 
             this.weaponSmashed = true;
+            this.weaponHits = true;
             this.weaponIncrease = 0;
             this.weaponLocation = new Point2D(event.getX(), event.getY());
             freezeInputs(Constant.WEAPON_ATTACK_ANIM);
@@ -170,7 +173,11 @@ public class MouseInputs implements EventHandler<MouseEvent> {
      * @return weaponSmashed
      */
     public boolean isWeaponSmashed() {
-        return this.weaponSmashed;
+        if (weaponHits) {
+            this.weaponHits = false;
+            return true;
+        }
+        return false;
     }
 
     /**
