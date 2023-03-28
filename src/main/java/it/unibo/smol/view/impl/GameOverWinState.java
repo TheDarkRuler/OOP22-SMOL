939a54c02c27749a0cdc1e0884e47d7a1a881261
Mounir Samite <mounir.samite@studio.unibo.it>
2023-03-29 00:59:03 +0200

    add UI elements

--- src/main/java/it/unibo/smol/view/impl/GameOverWinState.java
+++ src/main/java/it/unibo/smol/view/impl/GameOverWinState.java
@@ -1,5 +1,7 @@
 package it.unibo.smol.view.impl;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -7,13 +9,17 @@ import java.util.logging.Logger;
 import it.unibo.smol.view.GameMap;
 import it.unibo.smol.view.api.WindowState;
 import javafx.application.Platform;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.Border;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
-import javafx.scene.paint.Stop;
 import javafx.stage.Stage;
 
 public class GameOverWinState implements WindowState {
@@ -44,22 +50,25 @@ public class GameOverWinState implements WindowState {
      */
     private void start(final Stage stage) throws IOException {
         Platform.runLater(()->{
-            final var root = new Pane();
+            final var root = new StackPane();
             final var scene = new Scene(root, GameMap.WIDTH, GameMap.HEIGHT, Color.AQUA);
             final Button restart = new Button();
             final Button close = new Button();
+            ImageView gameOverGif = new ImageView();
+            try {
+                gameOverGif = new ImageView(new Image(new FileInputStream("src/main/resources/images/GAME_OVER.gif")));
+            } catch (FileNotFoundException e) {
+                Logger.getLogger(GameOverWinState.class.getName()).info("Illegal Argument");
+            }
             setupButton(close, "CLOSE");
             setupButton(restart, "RESTART");
-            restart.setText("RESTART");
             restart.setOnMouseClicked(e -> {
-                new WindowImpl().launch(stage);;
+                new WindowImpl().launch(stage);
             });
             close.setOnMouseClicked(e -> {
                 Platform.exit();
                 System.exit(0);
             });
-            restart.setLayoutX(GameMap.WIDTH / 2 - GameMap.BORDER_WIDTH * 2);
-            close.setLayoutX(GameMap.WIDTH / 2 );
             root.setOnKeyPressed(e -> {
                 if (e.getCode().equals(KeyCode.F11)) {
                     if (stage.isFullScreen()) {
@@ -69,8 +78,13 @@ public class GameOverWinState implements WindowState {
                     }
                 }
             });
+            root.setPrefSize(GameMap.MAP_WIDTH, GameMap.MAP_HEIGHT);
             root.getChildren().add(restart);
             root.getChildren().add(close);
+            root.getChildren().add(gameOverGif);
+            StackPane.setAlignment(gameOverGif, Pos.CENTER);
+            StackPane.setAlignment(restart, Pos.CENTER_LEFT);
+            StackPane.setAlignment(close, Pos.CENTER_RIGHT);
             stage.setTitle("Game Over :(");
             stage.setScene(scene);
             stage.setFullScreen(true);
