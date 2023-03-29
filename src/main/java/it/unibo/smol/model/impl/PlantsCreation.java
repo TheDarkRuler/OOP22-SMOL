5a13cd36a89d0f0504f062c04e95da024ec412f3
Marco Galeri <marco.galeri@studio.unibo.it>
2023-03-29 16:40:51 +0200

    add some precision

--- src/main/java/it/unibo/smol/model/impl/PlantsCreation.java
+++ src/main/java/it/unibo/smol/model/impl/PlantsCreation.java
@@ -43,20 +43,20 @@ public class PlantsCreation {
     private Point2D findPosition(final int i) {
         switch (i) {
             case 0 -> {
-                return new Point2D(plantRand(1+GameMap.BORDER_WIDTH, GameMap.MAP_WIDTH),
-                    plantRand(1+GameMap.BORDER_HEIGHT, GameMap.MAP_HEIGHT));
+                return new Point2D(plantRandX(1+GameMap.BORDER_WIDTH, GameMap.MAP_WIDTH),
+                    plantRandY(1+GameMap.BORDER_HEIGHT, GameMap.MAP_HEIGHT));
             }
             case 1 -> {
-                return new Point2D(plantRand(1+GameMap.WIDTH/2, GameMap.MAP_WIDTH),
-                    plantRand(1+GameMap.BORDER_HEIGHT, GameMap.HEIGHT/2));
+                return new Point2D(plantRandX(1+GameMap.WIDTH/2, GameMap.MAP_WIDTH),
+                    plantRandY(1+GameMap.BORDER_HEIGHT, GameMap.HEIGHT/2));
             }
             case 2 -> {
-                return new Point2D(plantRand(1+GameMap.BORDER_WIDTH, GameMap.WIDTH/2),
-                    plantRand(1+GameMap.HEIGHT/2, GameMap.MAP_HEIGHT));
+                return new Point2D(plantRandX(1+GameMap.BORDER_WIDTH, GameMap.WIDTH/2),
+                    plantRandY(1+GameMap.HEIGHT/2, GameMap.MAP_HEIGHT));
             } 
             case 3 -> {
-                return new Point2D(plantRand(1+GameMap.WIDTH/3, GameMap.WIDTH *2/3),
-                    plantRand(1+GameMap.HEIGHT/3, GameMap.HEIGHT*2/3));
+                return new Point2D(plantRandX(1+GameMap.WIDTH/3, GameMap.WIDTH *2/3),
+                    plantRandY(1+GameMap.HEIGHT/3, GameMap.HEIGHT*2/3));
             }
             default -> {
                 return null;
@@ -65,12 +65,18 @@ public class PlantsCreation {
     }
 
     /**
+     * finds a random position in the x axis.
+     * @return a random x position
+     */
+    private double plantRandX(double origin, double bound) {
+        return rand.nextDouble(origin+Constant.HEALTH_WIDTH / 2, bound- Constant.HEALTH_WIDTH / 2);
+    }
+
+    /**
      * finds a random position in the y axis.
      * @return a random y position
      */
-    private double plantRand(double origin, double bound) {
-        /*return GameMap.BORDER_HEIGHT + Constant.HEALTH_HEIGHT / 2
-            + rand.nextDouble(GameMap.MAP_HEIGHT - Constant.HEALTH_HEIGHT / 2) + bound;*/
-        return rand.nextDouble(origin, bound);
+    private double plantRandY(double origin, double bound) {
+        return rand.nextDouble(origin+Constant.HEALTH_HEIGHT / 2, bound- Constant.HEALTH_HEIGHT / 2);
     }
 }
