f109595d8a25701b611e0c1d777eee6c0b3d68f5
Giovanni Paradisi <giovanni.paradisi2@studio.unibo.it>
2023-03-31 14:20:31 +0200

    start testing

--- src/test/java/it/unibo/smol/model/WorldTest.java
+++ src/test/java/it/unibo/smol/model/WorldTest.java
@@ -1,5 +1,27 @@
 package it.unibo.smol.model;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import it.unibo.smol.model.impl.WorldImpl;
+
 class WorldTest {
 
+    private final WorldImpl world = new WorldImpl();
+
+    @Test
+    void testInitializeWorld() {
+        assertNotNull(world);
+        assertEquals(0, world.getScore());
+        final var entities = world.getEntities();
+        //Check if the entities deque is empty at the start
+        assertTrue(entities.isEmpty());
+        final var plants = world.getLifePlants();
+        //Check if the map of plants is empty at the start
+        assertEquals(0, plants.size());
+        
+    }
 }
