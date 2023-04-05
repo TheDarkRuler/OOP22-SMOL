1dc8a268d6fc9239262b4ad4189cabcfb792f4f2
Marco Galeri <marco.galeri@studio.unibo.it>
2023-04-05 17:06:37 +0200

    Health test

---
+++ src/test/java/it/unibo/smol/model/HealthTest.java
@@ -0,0 +1,50 @@
+package it.unibo.smol.model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import it.unibo.smol.model.impl.HealthComponent;
+
+public class HealthTest {
+
+    final int maxHealth = 10;
+    HealthComponent health = new HealthComponent(maxHealth);
+
+    @Test
+    void initializeHealthTest() {
+        assertEquals(maxHealth, health.getCurrentHealth()); 
+        assertFalse(health.isDead()); 
+    }
+
+    @Test
+    void decreaseHealthTest() {
+        health.setHealth(-1);
+        assertNotEquals(maxHealth, health.getCurrentHealth());
+        assertEquals(maxHealth-1, health.getCurrentHealth()); 
+        assertFalse(health.isDead()); 
+    }
+
+    @Test
+    void increaseHealthTest() {
+        health.setHealth(1);
+        assertNotEquals(maxHealth-1, health.getCurrentHealth());
+        assertEquals(maxHealth, health.getCurrentHealth()); 
+        assertFalse(health.isDead()); 
+    }
+
+    @Test
+    void MaxHealthTest() {
+    health.setHealth(1);
+        assertNotEquals(maxHealth+1, health.getCurrentHealth());
+    }
+
+    @Test
+    void DeathTest() {
+    health.setHealth(-maxHealth);
+        assertTrue(health.isDead());
+    }
+}
