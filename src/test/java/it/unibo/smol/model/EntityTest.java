76a0c6407245e59d3fc90b9f5dd3055d0918222e
Giovanni Paradisi <giovanni.paradisi2@studio.unibo.it>
2023-04-06 11:22:40 +0200

    Entity test

--- src/test/java/it/unibo/smol/model/EntityTest.java
+++ src/test/java/it/unibo/smol/model/EntityTest.java
@@ -1,25 +1,16 @@
 package it.unibo.smol.model;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 
-import it.unibo.smol.common.Constant;
-import it.unibo.smol.common.hitbox.RectangleHB;
-import it.unibo.smol.model.api.Entity;
 import it.unibo.smol.model.api.PhysicsComponent;
 import it.unibo.smol.model.impl.EntityFactoryImpl;
-import it.unibo.smol.model.impl.EntityImpl;
 import it.unibo.smol.model.impl.WorldImpl;
-import it.unibo.smol.model.impl.physicscomponent.EnemyPhysicsComponent;
-import it.unibo.smol.model.impl.physicscomponent.PlayerPhysicsComponent;
 import javafx.geometry.Point2D;
 
 /**
@@ -34,8 +25,16 @@ public class EntityTest {
         final var basicEnemy = new EntityFactoryImpl().createBasicEnemy(new Point2D(0, 0), world);
         //Check entity type
         assertEquals(Type.ENEMY, basicEnemy.getType());
-        //Check if physics component is not empty
-        assertNotEquals(Optional.empty(), basicEnemy.getPhysicsComp());
+        //Check physics component
+        assertTrue(basicEnemy.getPhysicsComp().isPresent());
+        assertTrue(PhysicsComponent.class.isInstance(basicEnemy.getPhysicsComp().get()));
+    }
 
+    @Test
+    void testEntityComponentIsEmpty() {
+        final var lifeplants = new EntityFactoryImpl().createLifePlants(0, 0, world);
+        //Check entity type
+        assertEquals(Type.HEALTH, lifeplants.getType());
+        assertTrue(lifeplants.getInputComp().isEmpty());
     }
 }
